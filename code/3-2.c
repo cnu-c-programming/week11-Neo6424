36bf6bb83ecd9ebec8a951d8fcdacc4e6b405154
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-05-15 09:26:43 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp_asc(const int* a, const int* b){
+    return(*a - *b);
+}
+
+int cmp_desc(const int* a, const int* b){
+    return(*b - *a);
+}
+
+int main(void) {
+    int arr[] = {5,2,8,1,9,3};
+    int size = 6;
+
+    qsort(&arr, size, sizeof(int), cmp_asc);
+    print_arr(arr, size);
+    qsort(&arr, size, sizeof(int), cmp_desc);
+    print_arr(arr, size);
+}
